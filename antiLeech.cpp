044d4b9cefc9fece14baad3595b168792978bd17
persmule <persmule@gmail.com>
2014-02-04 09:59:37 +0800

    re-enable Bill Lee's Ghost Mod check, for main program can always get peers' modstring now, if only it does exist.

--- antiLeech.cpp
+++ antiLeech.cpp
@@ -1414,7 +1414,8 @@ LPCTSTR __declspec(dllexport) DLPCheckNameAndHashAndMod(const CString& username,
 //Ghost Mod check [Bill Lee]
 	int Client_Data[] = {modversion.GetLength(), username.GetLength(), username.ReverseFind(91), /* username.ReverseFind(93), */ 0};
 	//[Persmule] Temporarily disable this check to avoid injustice, because amule cannot always extract modversion successfully from each hello packet.
-	#if 0
+	//[Persmule] Re-enable this check because the main program has a modstring of itself to exchange peers' ones.
+	
 	if (StrStrI(username, _T("speedyp2p.com")) || Client_Data[0]!=0 /* || Client_Data[1]<10 */ || !_tcsstr(username, _T("«")) && !_tcsstr(username, _T("»")))
 	{
 		;
@@ -1424,7 +1425,7 @@ LPCTSTR __declspec(dllexport) DLPCheckNameAndHashAndMod(const CString& username,
 			(int)username[Client_Data[3]-6]==32 && username.ReverseFind(_T('»')) == Client_Data[1]-1 && */
 		return _T("Ghost Mod"); //Their NickName look like a normal eMule Mods but without ModString
 	}
-	#endif
+	
 	//End
 
 //End
